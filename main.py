729ed22d5ded8c93102ce0e65a811a4f655fc755
el-iso <elijamesbarton@gmail.com>
2020-10-28 08:51:21 -0500

    parses list to only include rows with Mod Bus addresses

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import csv
+from enum import IntEnum
+import json
+
+
+class ADVariables(IntEnum):
+    SYSTEM_ID = 0
+    TAG_NAME = 1
+    MOD_BUS_ADDRESS = 4
+
 
 def read_csv(fileName):
     with open(fileName) as fp:
         reader = csv.reader(fp, delimiter=",", quotechar='"')
-        # next(reader, None)  # skip the headers
+        next(reader, None)  # skip the headers
         data_read = [row for row in reader]
 
     return data_read
 
+
 if __name__ == '__main__':
-    csv_data = read_csv("../Tags_01_extended.csv")
-    print(csv_data)
+    csv_data = list(read_csv("../Tags_01_extended.csv"))
+    csv_data.remove(csv_data[0])
 
+    usefulRowsOnly = [row for row in csv_data if not row[ADVariables.MOD_BUS_ADDRESS] == ""]
 
+    print(print(json.dumps(usefulRowsOnly)))
